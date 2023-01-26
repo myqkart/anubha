f4553c68b201c20f85f2b16ef50bd821bda08671
myqkart <yshop674@gmail.com>
2023-01-26 19:31:38 +0530

    status

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ export const Home = () => {
 
       <div className="content">
         <h2>Hi There,<br /> I'm {PERSON.first_name} <span>{PERSON.last_name}</span></h2>
-        <p>i am into <span className="typing-text">Web Development</span></p>
+        <p>i am into <span className="typing-text">Software Development</span></p>
         <Link to="/about" className="btn">
           <span>About Me</span>
           <i className="fas fa-arrow-circle-right"></i>
